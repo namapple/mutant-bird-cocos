64034f54e3b15b6754ef174de19b31dff2714380
NamDao <bk.namapple@gmail.com>
2023-05-22 22:58:45 +0700

    Reformat code

--- assets/Scripts/Bird.ts
+++ assets/Scripts/Bird.ts
@@ -1,74 +1,68 @@
-import {
-  _decorator,
-  CCFloat,
-  Component,
-  Node,
-  Vec3,
-  Animation,
-  tween,
-} from "cc";
-const { ccclass, property } = _decorator;
+import {_decorator, CCFloat, Component, Node, Vec3, Animation, tween} from "cc";
+
+const {ccclass, property} = _decorator;
 
 @ccclass("Bird")
 export class Bird extends Component {
-  @property({
-    type: CCFloat,
-    tooltip: "How high does he fly?",
-  })
-  public jumpHeight: number = 1.5;
 
-  @property({
-    type: CCFloat,
-    tooltip: "How long does he fly?",
-  })
-  public jumpDuration: number = 1.5;
+    @property({
+        type: CCFloat,
+        tooltip: "How high does he fly?",
+    })
+    public jumpHeight: number = 1.5;
 
-  // Animation property of the bird
-  public birdAnimation: Animation;
+    @property({
+        type: CCFloat,
+        tooltip: "How long does he fly?",
+    })
+    public jumpDuration: number = 1.5;
 
-  // Temporary location of the bird
-  public birdLocation: Vec3;
-  public hitSomething: boolean;
+    // Animation property of the bird
+    public birdAnimation: Animation;
 
-  // All the actions we want done when we start the script.
-  onLoad(): void {
-    // Restart the bird
-    this.resetBird();
-    // Get the initial information
-    this.birdAnimation = this.getComponent(Animation);
-  }
+    // Temporary location of the bird
+    public birdLocation: Vec3;
+    public hitSomething: boolean;
 
-  resetBird() {
-    // Create original bird location
-    this.birdLocation = new Vec3(0, 0, 0);
-    // Place bird in location
-    this.node.setPosition(this.birdLocation);
+    // All the actions we want done when we start the script.
+    onLoad(): void {
+        // Restart the bird
+        this.resetBird();
+        // Get the initial information
+        this.birdAnimation = this.getComponent(Animation);
+    }
 
-    this.hitSomething = false;
-  }
+    resetBird() {
+        // Create original bird location
+        this.birdLocation = new Vec3(0, 0, 0);
+        // Place bird in location
+        this.node.setPosition(this.birdLocation);
 
-  fly() {
-    // Stop the bird animation immediately
-    this.birdAnimation.stop();
-    // Start the movement of the bird
-    tween(this.node.position)
-      .to(
-        this.jumpDuration,
-        new Vec3(
-          this.node.position.x,
-          this.node.position.y + this.jumpHeight,
-          0
-        ),
-        {
-          easing: "smooth",
-          onUpdate: (target: Vec3, ratio: number) => {
-            this.node.position = target;
-          },
-        }
-      )
-      .start();
+        this.hitSomething = false;
+    }
 
-    // Play the bird animation
-    this.birdAnimation.play();
-  }
+    fly() {
+        // Stop the bird animation immediately
+        this.birdAnimation.stop();
+        // Start the movement of the bird
+        tween(this.node.position)
+            .to(
+                this.jumpDuration,
+                new Vec3(
+                    this.node.position.x,
+                    this.node.position.y + this.jumpHeight,
+                    0
+                ),
+                {
+                    easing: "smooth",
+                    onUpdate: (target: Vec3, ratio: number) => {
+                        this.node.position = target;
+                    },
+                }
+            )
+            .start();
+
+        // Play the bird animation
+        this.birdAnimation.play();
+    }
 }
